c90d7be4983cfd8793717e325ae52754ab939869
Maimeetang11 <boongnaja007@gmail.com>
2025-02-12 01:34:03 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ValentineDay from './ValentineDay'; // ตรวจสอบให้แน่ใจว่าไฟล์นี้มีอยู่จริง
+
+function App() {
+  return (
+    <div className="App">
+      <ValentineDay />
+    </div>
+  );
+}
+
+export default App;
